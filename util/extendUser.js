84ca2f9e2585bca2799176c3547b750ddacb1eba
Patrick Schmalstig <xanaftp@gmail.com>
2018-12-15 16:58:05 -0500

    No user settings
    Signed-off-by: Patrick Schmalstig <xanaftp@gmail.com>

--- util/extendUser.js
+++ util/extendUser.js
@@ -5,9 +5,6 @@ Structures.extend('User', User => class MyUser extends User {
     constructor(...args) {
         super(...args);
         
-        // User settings
-        this.settings = this.client.gateways.user.get(`${this.id}`, true);
-        
         // Guild based member settings
         this.guildSettings = function(guildID) {
           return this.client.gateways.members.create([guildID, this.id]);  
