4e4f6fb53c34492654ef3ca551ad91a77a1e2d39
Patrick Schmalstig <xanaftp@gmail.com>
2019-10-26 00:55:22 -0400

    mysql was a failure; go back to mongodb

--- events/klasaReady.js
+++ events/klasaReady.js
@@ -1,52 +1,69 @@
-const {Event} = require('klasa');
+const { Event } = require('klasa');
 const _ = require("lodash");
 
 module.exports = class extends Event {
 
-    run() {
+    run () {
 
         var updateLevels = (guildMember) => {
             // Update level roles
             var levelRoles = {};
             var levelRoles2 = guildMember.guild.settings.levelRoles;
-            for (var key in levelRoles2)
-            {
-                if (levelRoles2.hasOwnProperty(key))
-                {
-                    if (levelRoles2[key] === null)
+            for (var key in levelRoles2) {
+                if (levelRoles2.hasOwnProperty(key)) {
+                    if (levelRoles2[ key ] === null)
                         continue;
-                    levelRoles[key.replace('level', '')] = levelRoles2[key];
+                    levelRoles[ key.replace('level', '') ] = levelRoles2[ key ];
                 }
             }
             var levelKeys = Object.keys(levelRoles);
-            if (levelKeys.length > 0)
-            {
+            if (levelKeys.length > 0) {
                 var rolesToAdd = [];
                 var rolesToRemove = [];
                 levelKeys.map(levelKey => {
                     var xp = Math.ceil(((levelKey - 1) / 0.177) ** 2);
-                    if (guildMember.guild.roles.has(levelRoles[levelKey]))
-                    {
-                        if (guildMember.settings.xp >= xp && !guildMember.roles.has(levelRoles[levelKey]))
-                        {
-                            rolesToAdd.push(levelRoles[levelKey]);
-                        } else if (guildMember.settings.xp < xp && guildMember.roles.has(levelRoles[levelKey])) {
-                            rolesToRemove.push(levelRoles[levelKey]);
+                    if (guildMember.guild.roles.has(levelRoles[ levelKey ])) {
+                        if (guildMember.settings.xp >= xp && !guildMember.roles.has(levelRoles[ levelKey ])) {
+                            rolesToAdd.push(levelRoles[ levelKey ]);
+                        } else if (guildMember.settings.xp < xp && guildMember.roles.has(levelRoles[ levelKey ])) {
+                            rolesToRemove.push(levelRoles[ levelKey ]);
                         }
                     }
                 });
 
                 if (rolesToAdd.length > 0)
                     guildMember.roles.add(rolesToAdd, `Level Update (add roles)`)
-                            .then(stuff => {
-                                if (rolesToRemove.length > 0)
-                                    guildMember.roles.remove(rolesToRemove, `Level Update (remove roles)`);
-                            });
+                        .then(stuff => {
+                            if (rolesToRemove.length > 0)
+                                guildMember.roles.remove(rolesToRemove, `Level Update (remove roles)`);
+                        });
             }
         }
 
         // Iterate through guild operations on bot startup
         this.client.guilds.each(function (guild) {
+
+            // Kick self if the guild is black listed
+            if (!guild.available)
+                return;
+            if (this.client.settings.guildBlacklist.includes(guild.id)) {
+                guild.leave();
+                this.client.emit('warn', `Blacklisted guild detected: ${guild.name} [${guild.id}]`);
+                return;
+            }
+
+            // Add a scheduled task to run every minute for the guild if it does not already exist
+            const guildTask = guild.settings.guildTasks;
+            if (!guildTask || guildTask === null) {
+                this.client.schedule.create('guildtasks', "* * * * *", {
+                    data: {
+                        guild: guild.id,
+                    }
+                })
+                    .then((task) => {
+                        guild.settings.update('guildTasks', task.id);
+                    });
+            }
 
             // Cache the last (default #) messages in all channels
             guild.channels.each(function (channel) {
@@ -57,17 +74,15 @@ module.exports = class extends Event {
             // Cycle through all the members without the verified role and assign them the stored roles
             const verified = guild.settings.verifiedRole;
             const verifiedRole = guild.roles.get(verified);
-            if (verifiedRole)
-            {
+            if (verifiedRole) {
                 guild.members.each(function (guildMember) {
                     // Member has the verified role. Update database with the current roles set in case anything changed since bot was down.
-                    if (guildMember.roles.get(verifiedRole.id))
-                    {
+                    if (guildMember.roles.get(verifiedRole.id)) {
                         var roleArray = [];
                         guildMember.settings.reset(`roles`);
                         guildMember.roles.each(function (role) {
                             if (role.id !== guild.defaultRole.id)
-                                guildMember.settings.update(`roles`, role, guild, {action: 'add'});
+                                guildMember.settings.update(`roles`, role, guild, { action: 'add' });
                         });
                         updateLevels(guildMember);
                         // Member does not have verified role, so add all roles from the database
@@ -76,11 +91,10 @@ module.exports = class extends Event {
                         var _temp = guildMember.settings.roles;
                         var temp = _.cloneDeep(_temp);
                         guildMember.roles.add(temp, `Re-assigning saved roles`)
-                                .then(newMember => updateLevels(newMember));
+                            .then(newMember => updateLevels(newMember));
 
                         // Also if the guild is under a raid mitigation level 2+, assign the mitigation role to the new member.
-                        if (guildMember.guild.settings.raidMitigation > 1)
-                        {
+                        if (guildMember.guild.settings.raidMitigation > 1) {
                             const raidRole = guildMember.guild.roles.get(guildMember.guild.settings.raidRole);
                             if (raidRole)
                                 guildMember.roles.add(raidRole, `Raid mitigation is active`);
@@ -97,16 +111,16 @@ module.exports = class extends Event {
             const modLog = guild.settings.modLogChannel;
             const _channel = guild.channels.get(modLog);
             guild.fetchInvites()
-                    .then(invites => {
-                        invites
-                                .filter(invite => typeof invite.inviter === 'undefined' || invite.inviter === null)
-                                .each((invite) => {
-                                    invite.delete('This invite has no inviter. Maybe the inviter left the guild?');
-                                    if (modLog)
-                                        _channel.send(`:wastebasket: The invite ${invite.code} was deleted because an inviter did not exist. They probably left the guild.`);
-                                });
+                .then(invites => {
+                    invites
+                        .filter(invite => typeof invite.inviter === 'undefined' || invite.inviter === null)
+                        .each((invite) => {
+                            invite.delete('This invite has no inviter. Maybe the inviter left the guild?');
+                            if (modLog)
+                                _channel.send(`:wastebasket: The invite ${invite.code} was deleted because an inviter did not exist. They probably left the guild.`);
+                        });
 
-                    });
+                });
         });
 
 
