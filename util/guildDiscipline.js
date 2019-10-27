b45d0bbe9f5bd1dccf42fe4db831745775f7d6b4
Patrick Schmalstig <xanaftp@gmail.com>
2019-10-27 10:24:45 -0400

    Use split text instead of embeds for discipline

--- util/guildDiscipline.js
+++ util/guildDiscipline.js
@@ -1,6 +1,4 @@
 // This class constructs and issues a discipline against a guild member via the user extension
-
-const { MessageEmbed } = require('discord.js');
 const ModLog = require('./modLog');
 const moment = require('moment');
 
@@ -164,8 +162,94 @@ module.exports = class GuildDiscipline {
 
         var guildMember = this.guild.members.resolve(this.user.id);
 
-        // Init the message embed
-        const embed = new MessageEmbed();
+        // Init the message
+        const msg = ``; // Intro / reason
+        const msg2 = "The following discipline has been issued: \n\n"; // Discipline / Accountability
+        const msg3 = ``; // Appeals / Closing / not in the guild
+
+        // Update the incidents channel with relevant information
+        if (this.message !== null)
+            await this.message.delete();
+        switch (this.type) {
+            case 'warn':
+                msg += ":warning: **__You have been issued a formal warning__** :warning: \n\n"
+                msg += "Everyone wants to enjoy their time here. But something you did recently was a bit out of line. You are being issued a formal warning for the following: \n"
+                msg += `**${this.reason}**` + "\n\n"
+                msg += "Staff would like to see you work on this concern and learn from this encounter. Staff may need to issue discipline if this happens again."
+
+                msg2 = ""
+
+                msg3 += "If you need assistance, or have any questions or concerns about this warning, feel free to post in this private channel with staff. They will be happy to help you. But please remain respectful. \n"
+                msg3 += "**You have 48 hours to dispute this warning in this text channel if you feel it was wrongly issued**. Leaving the guild, being disrespectful towards staff, or trying to discuss the matter outside of this channel will automatically make this warning final and unappealable."
+                msg3 += "Thank you for your understanding and cooperation."
+
+                if (!guildMember)
+                    msg3 += "\n\n**User not in the guild**\nI will add permissions to this channel if/when I detect them re-entering the guild."
+
+                break;
+            case 'discipline':
+                msg += ":octagonal_sign: **__You have been issued discipline__** :octagonal_sign: \n\n"
+                msg += "We all make mistakes, but everyone wants to enjoy their experience here. Something you did recently was out of line. You are being issued discipline for the following: \n"
+                msg += `**${this.reason}**` + "\n\n"
+                msg += "Staff would like to see you work on this concern and learn from this encounter. Staff may need to issue more severe discipline if this happens again."
+
+                msg3 += "This channel is private between you and staff; you may communicate any questions or concerns you have here. If you need help resolving this incident, staff are happy to provide some tips and guidance. But please remain respectful. \n"
+                msg3 += "**You have 48 hours to dispute this discipline in this text channel if you feel it was wrongly issued**. Leaving the guild, being disrespectful towards staff, or trying to discuss the matter outside of this channel will automatically make this discipline final and unappealable."
+                msg3 += "Thank you for your understanding and cooperation."
+
+                if (!guildMember)
+                    msg3 += "\n\n**User not in the guild**\nI will add permissions to this channel if/when I detect them re-entering the guild."
+
+                break;
+            case 'mute':
+                msg += ":mute: **__You have been muted for a while__** :mute: \n\n"
+                msg += "We all make mistakes, but everyone wants to enjoy their experience here. Something you did recently was out of line. You are being issued an extended mute for the following: \n"
+                msg += `**${this.reason}**` + "\n\n"
+                msg += "Staff would like to see you work on this concern and learn from this encounter. Staff may need to issue more severe discipline if this happens again."
+
+                msg2 += "**Mute** \n"
+                msg2 += `You have been muted from the guild ${this.duration === 0 ? 'until staff manually remove the muted role from you' : `until ${moment().add(this.duration, 'minutes').format("LLLL Z")}`} (${this.duration} minutes from now)` + "\n\n"
+
+                msg3 += "This channel is private between you and staff; you may communicate any questions or concerns you have here. If you need help resolving this incident, staff are happy to provide some tips and guidance. But please remain respectful. \n"
+                msg3 += "**You have 48 hours to dispute this discipline in this text channel if you feel it was wrongly issued**. Leaving the guild, being disrespectful towards staff, or trying to discuss the matter outside of this channel will automatically make this discipline final and unappealable."
+                msg3 += "Thank you for your understanding and cooperation."
+
+                if (!guildMember)
+                    msg3 += "\n\n**User not in the guild**\nI will add the mute and permissions to this channel if/when I detect them re-entering the guild."
+
+                break;
+            case 'tempban':
+                msg += ":no_entry: **__You have been banned temporarily from the guild__** :no_entry: \n\n"
+                msg += "We know you can do better than this. We are disappointed we have had to go as far as to temp-ban you. You are being temp-banned for the following: \n"
+                msg += `**${this.reason}**` + "\n\n"
+                msg += "Staff would like to see you work on this concern and learn from this encounter. Staff may need to issue a permanent ban if this happens again."
+
+                msg2 += "**Temporary Ban** \n"
+                msg2 += `Once you leave the guild, a ban will be placed on you, which will be removed by the bot in ${this.duration / (60 * 24)} days. Your temp-ban time will not begin until you leave the guild or get kicked; until then, you will remain muted.` + "\n\n"
+
+                msg3 += "This channel is private between you and staff; you may communicate any questions or concerns you have here prior to leaving (once you leave, you will lose access to the server until the suspension ends). If you need help resolving this incident, staff are happy to provide some tips and guidance. But please remain respectful. \n"
+                msg3 += "**You have 48 hours to dispute this temp ban in this text channel if you feel it was wrongly issued**. Leaving the guild, being disrespectful towards staff, or trying to discuss the matter outside of this channel will automatically make this temp ban final and unappealable."
+                msg3 += "Thank you for your understanding and cooperation."
+
+                if (!guildMember)
+                    msg3 += "\n\n**User not in the guild**\nThe temp ban was applied immediately."
+                break;
+            case 'ban':
+                    msg += ":no_entry_sign: **__You have been banned from the guild indefinitely__** :no_entry_sign: \n\n"
+                    msg += "Your conduct in the guild cannot be tolerated any longer. Therefore, for the safety of the community, you are being indefinitely removed from the guild. We wish you the best in your adventures and hope you enjoyed your stay in this guild. You are being banned for the following: \n"
+                    msg += `**${this.reason}**`
+    
+                    msg2 += "**Ban** \n"
+                    msg2 += `Once you leave the guild, a server ban will be placed on you. This ban will remain in place indefinitely or until staff manually remove it. Until you leave or staff kick you, you will remain muted.` + "\n\n"
+    
+                    msg3 += "This channel is private between you and staff; you may communicate any questions or concerns you have here prior to leaving (once you leave, you will lose access to the guild). If you need help resolving this incident, staff are happy to provide some tips and guidance. But please remain respectful. \n"
+                    msg3 += "**You have 48 hours to dispute this ban in this text channel if you feel it was wrongly issued**. Leaving the guild, being disrespectful towards staff, or trying to discuss the matter outside of this channel will automatically make this ban final and unappealable."
+                    msg3 += "Thank you for your understanding and cooperation."
+    
+                    if (!guildMember)
+                        msg3 += "\n\n**User not in the guild**\nThe ban was applied immediately."
+                break;
+        }
 
         // prepare a modLog
         var modLog = await new ModLog(this.guild)
@@ -203,7 +287,9 @@ module.exports = class GuildDiscipline {
 
         // Issue discipline
         if (this.xp > 0) {
-            embed.addField(`You lost ${this.xp} XP`, `Your XP is now at ${(this.user.guildSettings(this.guild.id).xp - this.xp)}`);
+            msg2 += `**Loss of ${this.xp} XP**` + " \n"
+            msg2 += `Your XP is now at ${(this.user.guildSettings(this.guild.id).xp - this.xp)}` + "\n\n"
+
             await this.user.guildSettings(this.guild.id).update(`xp`, (this.user.guildSettings(this.guild.id).xp - this.xp));
 
             // Update level roles
@@ -243,15 +329,20 @@ module.exports = class GuildDiscipline {
             }
         }
         if (this.yang > 0) {
-            embed.addField(`You lost ${this.yang} Yang`, `Your Yang balance is now at ${(this.user.guildSettings(this.guild.id).yang - this.yang)}`);
+            msg2 += `**Loss of ${this.yang} Yang**` + " \n"
+            msg2 += `Your Yang balance is now at ${(this.user.guildSettings(this.guild.id).yang - this.yang)}` + "\n\n"
+
             this.user.guildSettings(this.guild.id).update(`yang`, (this.user.guildSettings(this.guild.id).yang - this.yang));
         }
         if (this.reputation > 0) {
-            embed.addField(`${this.reputation} bad reputation was assessed`, `Your reputation is now +${this.user.guildSettings(this.guild.id).goodRep} / -${(this.user.guildSettings(this.guild.id).badRep + this.reputation)}`);
+            msg2 += `**${this.reputation} bad reputation added to profile**` + " \n"
+            msg2 += `Your reputation is now ${this.user.guildSettings(this.guild.id).goodRep} good / ${(this.user.guildSettings(this.guild.id).badRep + this.reputation)} bad` + "\n\n"
+
             this.user.guildSettings(this.guild.id).update(`badRep`, (this.user.guildSettings(this.guild.id).badRep + this.reputation));
         }
         if (this.other !== null) {
-            embed.addField(`Other Discipline / Instructions`, `${this.other}`);
+            msg2 += `**Accountability / Additional Discipline**` + " \n"
+            msg2 += `${this.other}` + "\n\n"
         }
 
         // If the member is no longer in the guild, issue the ban or tempban immediately, and undo the mute
@@ -303,90 +394,11 @@ module.exports = class GuildDiscipline {
                 await this.guild.settings.update('pendBans', { user: this.user.id, reason: this.reason, channel: this.channel.id, case: modLog }, { action: 'add' });
         }
 
-        // Update the incidents channel with relevant information
-        if (this.message !== null)
-            await this.message.delete();
-        switch (this.type) {
-            case 'warn':
-                embed
-                    .setTitle(`:warning: You have been issued a warning :warning:`)
-                    .setDescription(`Everyone wants to enjoy their time here. But something you did recently was a bit out of line.
-Please review the below information. Staff would like to see you work on this concern and learn from this encounter. Staff may need to issue discipline if this happens again.           
-If you need assistance, or have any questions or concerns about this warning, feel free to post in this private channel with staff. They will be happy to help you. But please remain respectful.
-**You have 48 hours to dispute this warning in this text channel if you feel it was wrongly issued**. Leaving the guild, being disrespectful towards staff, or trying to discuss the matter outside of this channel will automatically make this warning final and unappealable.
-Thank you for your understanding and cooperation.`)
-                    .setColor(16564545)
-                    .setFooter(`Reason for the warning: ${this.reason}`);
-
-                if (!guildMember)
-                    embed.addField(`This user is not currently in the guild`, `I will add permissions to this channel if/when I detect them re-entering the guild.`);
-
-                await this.channel.send(`<@!${this.user.id}>`, { embed });
-                break;
-            case 'discipline':
-                embed
-                    .setTitle(`:octagonal_sign: You have been issued discipline :octagonal_sign:`)
-                    .setDescription(`We all make mistakes, but everyone wants to enjoy their experience here. Something you did recently was out of line. Please review the below information carefully, and work on improving your conduct.
-This channel is private between you and staff; you may communicate any questions or concerns you have here. If you need help resolving this incident, staff are happy to provide some tips and guidance. But please remain respectful.
-**You have 48 hours to dispute this discipline in this text channel if you feel it was wrongly issued**. Leaving the guild, being disrespectful towards staff, or trying to discuss the matter outside of this channel will automatically make this discipline final and unappealable.
-Thank you for your understanding and cooperation.`)
-                    .setColor(8421631)
-                    .setFooter(`Reason for discipline: ${this.reason}`);
-
-                if (!guildMember)
-                    embed.addField(`This user is not currently in the guild`, `I will add permissions to this channel if/when I detect them re-entering the guild.`);
-
-                await this.channel.send(`<@!${this.user.id}>`, { embed });
-                break;
-            case 'mute':
-                embed
-                    .setTitle(`:mute: You have been muted :mute:`)
-                    .setDescription(`We all make mistakes, but everyone wants to enjoy their experience here. Something you did recently was out of line. Please review the below information carefully, and work on improving your conduct.
-This channel is private between you and staff; you may communicate any questions or concerns you have here. If you need help resolving this incident, staff are happy to provide some tips and guidance. But please remain respectful.
-**You have until the mute expires to dispute it in this text channel if you feel it was wrongly issued**. Leaving the guild, being disrespectful towards staff, or trying to discuss the matter outside of this channel will automatically make this mute final and unappealable.
-Thank you for your understanding and cooperation.`)
-                    .addField(`Duration of Mute`, `The mute will expire ${this.duration === 0 ? 'when staff manually remove the muted role from you' : `at ${moment().add(this.duration, 'minutes').format("LLLL Z")}`} (${this.duration} minutes)`)
-                    .setColor(15014476)
-                    .setFooter(`Reason for mute: ${this.reason}`);
-
-                if (!guildMember)
-                    embed.addField(`This user is not currently in the guild`, `The mute will be applied if they re-enter the guild before it expires. I will add permissions to this channel if/when I detect them re-entering the guild.`);
-
-                await this.channel.send(`<@!${this.user.id}>`, { embed });
-                break;
-            case 'tempban':
-                embed
-                    .setTitle(`:no_entry: You have been suspended from the guild temporarily :no_entry:`)
-                    .setDescription(`We know you can do better than this. We are disappointed in your recent conduct. Please read the below information carefully and use this suspension time to reflect and improve your conduct.
-This channel is private between you and staff; you may communicate any questions or concerns you have here prior to leaving (once you leave, you will lose access to the server until the suspension ends). If you need help resolving this incident, staff are happy to provide some tips and guidance. But please remain respectful.
-**You have 48 hours to dispute this suspension in this text channel if you feel it was wrongly issued**. Leaving the guild, being disrespectful towards staff, or trying to discuss the matter outside of this channel will automatically make this suspension final and unappealable.
-Thank you for your understanding and cooperation.`)
-                    .addField(`Suspension Duration / Procedure`, `Once you leave the guild, a ban will be placed on you, which will be removed by the bot in ${this.duration / (60 * 24)} days. Your suspension time will not begin until you leave the guild or get kicked; until then, you will remain muted.`)
-                    .setColor(16573465)
-                    .setFooter(`Reason for suspension: ${this.reason}`);
-
-                if (!guildMember)
-                    embed.addField(`This user is not currently in the guild`, `The suspension was applied immediately.`);
-
-                await this.channel.send(`<@!${this.user.id}>`, { embed });
-                break;
-            case 'ban':
-                embed
-                    .setTitle(`:no_entry_sign: You have been banned from the guild indefinitely :no_entry_sign:`)
-                    .setDescription(`Your conduct in the guild cannot be tolerated any longer. Therefore, for the safety of the community, you are asked to leave the guild indefinitely. We wish you the best in your adventures and hope you enjoyed your stay in this guild.
-This channel is private between you and staff; you may communicate any questions or concerns you have here prior to leaving (once you leave, you will lose access to the guild).
-**You have 48 hours to dispute this ban in this text channel if you feel it was wrongly issued**. Leaving the guild, being disrespectful towards staff, or trying to discuss the matter outside of this channel will automatically make this ban final and unappealable.
-Thank you for your understanding and cooperation.`)
-                    .addField(`Ban Procedure`, `Once you leave the guild, a server ban will be placed on you. This ban will remain in place indefinitely or until staff manually remove it. Until you leave or staff kick you, you will remain muted.`)
-                    .setColor(16724253)
-                    .setFooter(`Reason for ban: ${this.reason}`);
-
-                if (!guildMember)
-                    embed.addField(`This user is not currently in the guild`, `The ban was applied immediately.`);
-
-                await this.channel.send(`<@!${this.user.id}>`, { embed });
-                break;
-        }
+        // Push out discipline message
+        this.channel.send(msg + "\n\n" + msg2 + msg3, {
+            split: true,
+            reply: this.user
+        })
 
         return this;
     }
