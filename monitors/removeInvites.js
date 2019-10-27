f87a721179dfec47bbb402b29e0155d262a292d8
Patrick Schmalstig <xanaftp@gmail.com>
2019-10-27 14:39:18 -0400

    exclude staff from link removal

--- monitors/removeInvites.js
+++ monitors/removeInvites.js
@@ -17,9 +17,15 @@ module.exports = class extends Monitor {
 
     run(message) {
         // Delete discord invites
-        if (/(https?:\/\/)?(www\.)?(discord\.(gg|li|me|io)|discordapp\.com\/invite)\/.+/.test(message.content) && !message.member.roles.get(message.guild.settings.modRole))
+        const { permission } = await this.client.permissionLevels.run(message, 4);
+        if (/(https?:\/\/)?(www\.)?(discord\.(gg|li|me|io)|discordapp\.com\/invite)\/.+/.test(message.content) && !permission)
         {
-            message.channel.send(`:x: <@${message.author.id}>, please contact a staff member if you want to promote a server.`);
+            message.channel.send(`:x: <@${message.author.id}>, please contact a staff member if you want to promote a server.`)
+            .then((msg) => {
+                setTimeout(function () {
+                    msg.delete();
+                }, 10000);
+            });
             message.delete();
         }
     }
--- monitors/removeLinks.js
+++ monitors/removeLinks.js
@@ -1,10 +1,9 @@
-const {Monitor} = require('klasa');
+const { Monitor } = require('klasa');
 
 module.exports = class extends Monitor {
 
     constructor(...args) {
         super(...args, {
-            name: 'responder',
             enabled: true,
             ignoreBots: false,
             ignoreSelf: true,
@@ -16,23 +15,23 @@ module.exports = class extends Monitor {
         });
     }
 
-    run(message) {
+    run (message) {
         if (message.type !== 'DEFAULT')
             return null;
-        if (/(https?:\/\/[^\s]+)/g.test(message.content) && message.member.settings.xp < 128)
-        {
+        const { permission } = await this.client.permissionLevels.run(message, 4);
+        if (/(https?:\/\/[^\s]+)/g.test(message.content) && message.member.settings.xp < 128 && !permission) {
             message.channel.send(`:x: <@${message.author.id}>, You must be level 3 (128 XP) or above to post links in this guild.`)
-                    .then((msg) => {
-                        setTimeout(function () {
-                            msg.delete();
-                        }, 10000);
-                    });
+                .then((msg) => {
+                    setTimeout(function () {
+                        msg.delete();
+                    }, 10000);
+                });
             message.delete();
             return null;
         }
     }
 
-    async init() {
+    async init () {
     }
 
 };
