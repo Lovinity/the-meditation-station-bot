6c35342877813b4526f8d85d61276ebe9fd2df3b
Patrick Schmalstig <xanaftp@gmail.com>
2019-11-01 02:29:32 -0400

    try JSON.parse

--- events/klasaReady.js
+++ events/klasaReady.js
@@ -88,8 +88,8 @@ module.exports = class extends Event {
                         // Member does not have verified role, so add all roles from the database
                     } else {
                         // We have to lodash clone the roles before we start adding them, otherwise guildMemberUpdate will interfere with this process
-                        var _temp = guildMember.settings.roles;
-                        console.log(typeof _temp)
+                        // BUG: some reason, roles return as a JSON string instead of an actual array.
+                        var _temp = JSON.parse(guildMember.settings.roles);
                         var temp = _.cloneDeep(_temp);
                         guildMember.roles.add(temp, `Re-assigning saved roles`)
                             .then(newMember => updateLevels(newMember));
