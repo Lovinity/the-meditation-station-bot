0c90df319e36622ab3a17279e68f877f5692f915
Patrick Schmalstig <xanaftp@gmail.com>
2019-11-03 15:48:50 -0500

    bug

--- commands/Social/rep.js
+++ commands/Social/rep.js
@@ -37,7 +37,7 @@ module.exports = class extends Command {
 
         // Purchase repping
         if (await yangStore(message, 'repMember', 1)) {
-            user.guildSettings(message.guild.id).update(`goodRep`, user.guildSettings(message.guild.id).settings.goodRep + 10)
+            user.guildSettings(message.guild.id).update(`goodRep`, user.guildSettings(message.guild.id).goodRep + 10)
             const channel2 = message.guild.channels.resolve(message.guild.settings.modLogChannel);
             if (channel2) {
                 channel2.send(`:rep: User ${message.author.tag} (${message.author.id}) repped ${user.tag} (${user.id}) for the following reason: ${reason}`);
