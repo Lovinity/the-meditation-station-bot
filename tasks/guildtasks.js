fbdfab5b63762e445b323c4322bb7cbe398a2f7b
Patrick Schmalstig <xanaftp@gmail.com>
2019-11-03 02:17:23 -0500

    Adjust anti-raid stat; anti-raid score adjustments

--- tasks/guildtasks.js
+++ tasks/guildtasks.js
@@ -1,14 +1,13 @@
-const {Task} = require('klasa');
+const { Task } = require('klasa');
 const moment = require("moment");
 const needle = require("needle");
-const {MessageEmbed} = require('discord.js');
+const { MessageEmbed } = require('discord.js');
 
 module.exports = class extends Task {
 
-    async run( { guild }) {
+    async run ({ guild }) {
         const _guild = this.client.guilds.resolve(guild);
-        if (_guild)
-        {
+        if (_guild) {
             // Antispam cooldown
             var cooldown = _guild.settings.antispamCooldown;
             _guild.members.each((guildMember) => {
@@ -22,25 +21,51 @@ module.exports = class extends Task {
             const statsMessageChannel = _guild.settings.statsMessageChannel;
             const statsMessage = _guild.settings.statsMessage;
 
-            if (statsMessage && statsMessageChannel)
-            {
+            if (statsMessage && statsMessageChannel) {
 
                 // Edit the message containing stats
-                var raidMitigation = `0 (Not Activated)`;
-                if (_guild.settings.raidScore === 1)
-                    raidMitigation = `1 (Verified Phone Number required for New Members)`;
-                if (_guild.settings.raidScore === 2)
-                    raidMitigation = `2 (Verified Phone Number required for New Members; New Members Isolated)`;
-                if (_guild.settings.raidScore === 3)
-                    raidMitigation = `3 (Verified Phone Number required for New Members; New Members isolated; All Invite Links Deleted)`;
+                var raidMitigation = `Anti-raid: `;
+                var score = _guild.settings.raidScore;
+                var minuses = 9;
+                while (score > 0) {
+                    minuses--;
+                    score -= 20;
+                    raidMitigation = `${raidMitigation} :warning: `
+                    if (minuses === 6) {
+                        raidMitigation = `${raidMitigation} :one: `
+                    } else if (minuses === 3) {
+                        raidMitigation = `${raidMitigation} :two: `
+                    } else if (minuses === 0) {
+                        raidMitigation = `${raidMitigation} :three: `
+                    }
+                }
+                while (minuses > 0) {
+                    minuses--;
+                    raidMitigation = `${raidMitigation} :heavy_minus_sign: `
+                    if (minuses === 6) {
+                        raidMitigation = `${raidMitigation} :one: `
+                    } else if (minuses === 3) {
+                        raidMitigation = `${raidMitigation} :two: `
+                    } else if (minuses === 0) {
+                        raidMitigation = `${raidMitigation} :three: `
+                    }
+                }
+                if (_guild.settings.raidMitigation === 0)
+                    raidMitigation = `${raidMitigation} **Level 0 (No mitigation; spamming = 30-minute mute)** `
+                if (_guild.settings.raidMitigation === 1)
+                    raidMitigation = `${raidMitigation} **Level 1 (Verified Phone Numbers for new members; spamming = untimed mute)** `
+                if (_guild.settings.raidMitigation === 2)
+                    raidMitigation = `${raidMitigation} **Level 2 (Verified Phone Numbers + guild isolation for new members; spamming = 24-hour ban)** `
+                if (_guild.settings.raidMitigation === 3)
+                    raidMitigation = `${raidMitigation} **Level 3 (All invite links deleted; verified Phone Numbers + guild isolation for new members; spamming = permanent ban)** `
                 var themessage = `:chart_with_upwards_trend: **Current ${_guild.name} Statistics** (edited automatically every minute) :chart_with_upwards_trend: \n\n`;
                 themessage = themessage + `Current Guild Time:  **${moment().format('LLLL')}** \n`;
                 themessage = themessage + `Number of members in the guild: **${_guild.members.array().length}** \n`;
-                themessage = themessage + `Anti-raid mitigation: **${raidMitigation}** \n`;
+                themessage = themessage + `${raidMitigation} \n`;
 
 
                 _guild.channels.resolve(statsMessageChannel).messages.fetch(statsMessage)
-                        .then(message => message.edit(themessage));
+                    .then(message => message.edit(themessage));
             }
 
             // Do icebreakers
@@ -48,30 +73,27 @@ module.exports = class extends Task {
             var m = n.getMinutes();
             var h = n.getHours();
 
-            if (m === 0 && (h === 6 || h === 12 || h === 18 || h === 0))
-            {
+            if (m === 0 && (h === 6 || h === 12 || h === 18 || h === 0)) {
                 const iceBreakerChannel = _guild.settings.iceBreakerChannel;
                 const _channel = this.client.channels.resolve(iceBreakerChannel);
-                if (_channel)
-                {
+                if (_channel) {
                     var iceBreakers = _guild.settings.icebreakers;
                     _channel.send(`:snowflake: **Time for another ice breaker question!** :snowflake:
                     
-${iceBreakers[Math.floor(Math.random() * iceBreakers.length)]}
+${iceBreakers[ Math.floor(Math.random() * iceBreakers.length) ]}
 `);
                 }
 
             }
 
             // Delete all invites if under mitigation level 3
-            if (_guild && _guild.settings.raidMitigation >= 3)
-            {
+            if (_guild && _guild.settings.raidMitigation >= 3) {
                 _guild.fetchInvites()
-                        .then(invites => {
-                            invites.each(invite => {
-                                invite.delete();
-                            });
-                        })
+                    .then(invites => {
+                        invites.each(invite => {
+                            invite.delete();
+                        });
+                    })
             }
 
             // Raid score cool-down
@@ -81,15 +103,12 @@ ${iceBreakers[Math.floor(Math.random() * iceBreakers.length)]}
             _guild.settings.update('raidScore', newscore);
 
             // Raid mitigation ends
-            if (newscore <= 0 && _guild.settings.raidMitigation > 0)
-            {
+            if (newscore <= 0 && _guild.settings.raidMitigation > 0) {
                 // Remove raidRole
                 _guild.members.each((guildMember) => {
                     var raidRole = _guild.roles.resolve(_guild.settings.raidRole);
-                    if (raidRole)
-                    {
-                        if (guildMember.roles.get(raidRole.id))
-                        {
+                    if (raidRole) {
+                        if (guildMember.roles.get(raidRole.id)) {
                             guildMember.roles.remove(raidRole, `Raid mitigation expired`);
                         }
                     }
@@ -101,8 +120,7 @@ ${iceBreakers[Math.floor(Math.random() * iceBreakers.length)]}
                 // Send announcement
                 var channel = _guild.settings.announcementsChannel;
                 const _channel = this.client.channels.resolve(channel);
-                if (_channel)
-                {
+                if (_channel) {
                     var response = `:ballot_box_with_check: **Raid mitigation has ended** :ballot_box_with_check: 
 
 I do not detect raid activity anymore. Raid mitigation has ended. Thank you for your patience.
@@ -119,90 +137,85 @@ ${_guild.settings.raidMitigation >= 3 ? `**Please remember to re-generate invite
 
             // Check for voice channel listening and award XP for listeners
             _guild.channels
-                    .filter((channel) => channel.type === 'voice')
-                    .each((channel) => {
-                        var award = false;
-                        var awardTo = [];
-                        channel.members
-                                .each((guildMember) => {
-                                    // Is the member undeaf and not a bot? They deserve a listening award!
-                                    if (!guildMember.voice.deaf && !guildMember.user.bot)
-                                        awardTo.push(guildMember);
-                                    // Is the member unmuted and not a bot? Award listening XP/Yang to qualified members (if no one is unmuted, no one gets rewarded)
-                                    // Every 2 minutes, allow for XP rewarding even if the only speaking member is a bot
-                                    if (!guildMember.voice.mute && (!guildMember.user.bot || m % 2 === 0))
-                                        award = true;
-                                });
+                .filter((channel) => channel.type === 'voice')
+                .each((channel) => {
+                    var award = false;
+                    var awardTo = [];
+                    channel.members
+                        .each((guildMember) => {
+                            // Is the member undeaf and not a bot? They deserve a listening award!
+                            if (!guildMember.voice.deaf && !guildMember.user.bot)
+                                awardTo.push(guildMember);
+                            // Is the member unmuted and not a bot? Award listening XP/Yang to qualified members (if no one is unmuted, no one gets rewarded)
+                            // Every 2 minutes, allow for XP rewarding even if the only speaking member is a bot
+                            if (!guildMember.voice.mute && (!guildMember.user.bot || m % 2 === 0))
+                                award = true;
+                        });
 
-                        // Award XP to everyone who qualifies if the channel as a whole qualifies
-                        if (award && awardTo.length > 1)
-                        {
-                            awardTo.forEach((guildMember) => {
-                                guildMember.xp(1);
-                            });
-                        }
-                    });
+                    // Award XP to everyone who qualifies if the channel as a whole qualifies
+                    if (award && awardTo.length > 1) {
+                        awardTo.forEach((guildMember) => {
+                            guildMember.xp(1);
+                        });
+                    }
+                });
 
             // tri-hourly Trivia Game at minute 58
-            if (m === 58 && h % 3 === 0)
-            {
+            if (m === 58 && h % 3 === 0) {
                 console.log(`trivia`);
                 const botGamesChannel = _guild.settings.botGamesChannel;
                 const _channel = this.client.channels.resolve(botGamesChannel);
-                if (_channel)
-                {
+                if (_channel) {
                     console.log(`channel`);
-                    needle('get', 'http://jservice.io/api/random', {}, {json: true})
-                            .then((response) => {
-                                console.log(`response`);
-                                response = response.body[0];
-                                if (response && response.invalid_count === null)
-                                {
-                                    console.log(`Valid input`);
-                                    var category = response.category.title;
-                                    var clue = response.question;
-                                    var answer = response.answer;
-                                    var yang = response.value !== null ? (response.value / 10) : 20;
+                    needle('get', 'http://jservice.io/api/random', {}, { json: true })
+                        .then((response) => {
+                            console.log(`response`);
+                            response = response.body[ 0 ];
+                            if (response && response.invalid_count === null) {
+                                console.log(`Valid input`);
+                                var category = response.category.title;
+                                var clue = response.question;
+                                var answer = response.answer;
+                                var yang = response.value !== null ? (response.value / 10) : 20;
 
-                                    var embed = new MessageEmbed()
-                                            .setTitle(`Trivia Contest!`)
-                                            .setDescription(`The first person to answer this question correctly will win **${yang}** Yang! But hurry... you only have 3 minutes to answer! Make your guesses as messages. I will respond if and only if you have the correct answer.`)
-                                            .setColor("GREEN")
-                                            .addField('Category', category)
-                                            .addField('Clue', clue);
+                                var embed = new MessageEmbed()
+                                    .setTitle(`Trivia Contest!`)
+                                    .setDescription(`The first person to answer this question correctly will win **${yang}** Yang! But hurry... you only have 3 minutes to answer! Make your guesses as messages. I will respond if and only if you have the correct answer.`)
+                                    .setColor("GREEN")
+                                    .addField('Category', category)
+                                    .addField('Clue', clue);
 
-                                    _channel.send({embed: embed});
-                                    _channel.awaitMessages(message => slugify(message.cleanContent).includes(slugify(answer)),
-                                            {max: 1, time: 180000, errors: ['time']})
-                                            .then(messages => {
-                                                messages.first().member.settings.update('yang', messages.first().member.settings.yang + yang);
-                                                _channel.send(`:first_place: Congratulations to <@${messages.first().member.id}> who got the answer correct! It was ${answer}. You just earned ${yang} Yang.`);
-                                            })
-                                            .catch(() => {
-                                                _channel.send(`:hourglass: Time is up! The answer was ${answer}.`)
-                                            });
-                                }
-                            })
-                            .catch(function (err) {
-                                console.error(err);
-                            })
+                                _channel.send({ embed: embed });
+                                _channel.awaitMessages(message => slugify(message.cleanContent).includes(slugify(answer)),
+                                    { max: 1, time: 180000, errors: [ 'time' ] })
+                                    .then(messages => {
+                                        messages.first().member.settings.update('yang', messages.first().member.settings.yang + yang);
+                                        _channel.send(`:first_place: Congratulations to <@${messages.first().member.id}> who got the answer correct! It was ${answer}. You just earned ${yang} Yang.`);
+                                    })
+                                    .catch(() => {
+                                        _channel.send(`:hourglass: Time is up! The answer was ${answer}.`)
+                                    });
+                            }
+                        })
+                        .catch(function (err) {
+                            console.error(err);
+                        })
                 }
             }
-    }
+        }
     }
 
 };
 
-function slugify(text)
-{
+function slugify (text) {
     return text.toString().toLowerCase()
-            .replace(/^(a |an |the |his |her |their |your |it's |its)/, '')        // Remove a, an, the, his, her, their, your, it's, its from the beginning
-            .replace(/ *\([^)]*\) */g, '')    // Remove anything in parenthesis
-            .replace(/(\<i\>|\<\/i\>)/g, '')    // Remove <i> and </i> HTML entities
-            .replace('&', 'and')                // Replace & with "and"
-            .replace(/\s+/g, '-')           // Replace spaces with -
-            .replace(/[^\w\-]+/g, '')       // Remove all non-word chars
-            .replace(/\-\-+/g, '-')         // Replace multiple - with single -
-            .replace(/^-+/, '')             // Trim - from start of text
-            .replace(/-+$/, '');            // Trim - from end of text
+        .replace(/^(a |an |the |his |her |their |your |it's |its)/, '')        // Remove a, an, the, his, her, their, your, it's, its from the beginning
+        .replace(/ *\([^)]*\) */g, '')    // Remove anything in parenthesis
+        .replace(/(\<i\>|\<\/i\>)/g, '')    // Remove <i> and </i> HTML entities
+        .replace('&', 'and')                // Replace & with "and"
+        .replace(/\s+/g, '-')           // Replace spaces with -
+        .replace(/[^\w\-]+/g, '')       // Remove all non-word chars
+        .replace(/\-\-+/g, '-')         // Replace multiple - with single -
+        .replace(/^-+/, '')             // Trim - from start of text
+        .replace(/-+$/, '');            // Trim - from end of text
 }
--- util/extendGuild.js
+++ util/extendGuild.js
@@ -29,19 +29,19 @@ Structures.extend('Guild', Guild => class MyGuild extends Guild {
     
 I have detected a potential raid. As a precaution, level 1 mitigation has been activated. All new members must have a verified phone number associated with their account until mitigation expires. In addition, antispam mutes issued during level 1 mitigation will last until staff manually remove them instead of 30 minutes. If no more activity is detected, mitigation should end in about one hour. If raid activity continues, higher levels will be activated.`);
                             });
-                } else if (newScore >= 90 && mitigation < 2)
+                } else if (newScore >= 120 && mitigation < 2)
                 {
                     this.settings.update('raidMitigation', 2);
                     if (_channel)
                         _channel.send(`:rotating_light: **Raid mitigation level 2 activated** :rotating_light:
     
-I continue to detect raid activity. As a further precaution, level 2 mitigation has been activated. All new members will be isolated in a channel with staff until mitigation ends. In addition, triggering the antispam system during level 2 mitigation will result in an automatic 1-day suspension. If no more raid-like activity is detected, mitigation should end in about 1.5 hours. If raid activity continues, higher levels will be activated.`);
-                } else if (newScore >= 120 && mitigation < 3) {
+I continue to detect raid activity. As a further precaution, level 2 mitigation has been activated. All new members will be isolated in a channel with staff until mitigation ends. In addition, triggering the antispam system during level 2 mitigation will result in an automatic 1-day suspension. If no more raid-like activity is detected, mitigation should end in about 2 hours. If raid activity continues, higher levels will be activated.`);
+                } else if (newScore >= 180 && mitigation < 3) {
                     this.settings.update('raidMitigation', 3);
                     if (_channel)
                         _channel.send(`@everyone :rotating_light: **Raid mitigation level 3 activated - All invite links deleted** :rotating_light:
     
-Serious raid activity continues to be detected. I activated the highest mitigation level. All invite links have been deleted, and any new invite links created before mitigation ends will also get deleted. In addition, triggering the antispam during level 3 mitigation will result in an automatic permanent ban. If no more raid activity is detected, mitigation should end in about 2 hours. Please do not forget to re-generate your invite links after mitigation ends.`);
+Serious raid activity continues to be detected. I activated the highest mitigation level. All invite links have been deleted, and any new invite links created before mitigation ends will also get deleted. In addition, triggering the antispam during level 3 mitigation will result in an automatic permanent ban. If no more raid activity is detected, mitigation should end in about 3 hours. Please do not forget to re-generate your invite links after mitigation ends.`);
                     this.fetchInvites()
                             .then(invites => {
                                 invites.each(invite => {
