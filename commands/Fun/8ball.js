b7214dd0eb5e671ea345b468dbf5612d742d261a
Patrick Schmalstig <xanaftp@gmail.com>
2019-11-05 01:21:17 -0500

    8ball

---
+++ commands/Fun/8ball.js
@@ -0,0 +1,46 @@
+// Copyright (c) 2017-2019 dirigeants. All rights reserved. MIT license.
+const { Command } = require('klasa');
+
+module.exports = class extends Command {
+
+    constructor(...args) {
+        super(...args, {
+            aliases: [ '8', 'magic', '8ball', 'mirror' ],
+            description: 'Magic 8-Ball, does exactly what the toy does.',
+            usage: '<query:str>'
+        });
+    }
+
+    run (msg, [ question ]) {
+        if (question.endsWith('?') && await yangStore(message, '8ball', 1)) {
+            return msg.reply(`🎱 ${answers[ Math.floor(Math.random() * answers.length) ]}`);
+        }
+        return msg.reply("🎱 That doesn't look like a question, try again please.");
+    }
+
+};
+
+const answers = [
+    'I do not know.',
+    'Definitely not.',
+    'I hope so.',
+    'Sorry, not in your wildest dreams.',
+    'It is possible.',
+    'Pretty likely.',
+    'I think so.',
+    'I hope not, lmao.',
+    'Never!',
+    'Nope, forget about it.',
+    'Are you actually asking me that?',
+    'Definitely!',
+    'Oh, absolutely!',
+    'No, absolutely not!',
+    'The future is bleak.',
+    'I am uncertain about that.',
+    'I would rather not say.',
+    'Why does that matter anyway?',
+    'Possibly.',
+    'If you get lucky.',
+    'There is a small chance.',
+    'Yes!'
+];
