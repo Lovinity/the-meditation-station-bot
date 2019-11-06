84b75f0bd888b061b272d1c117c01ca813268df5
Patrick Schmalstig <xanaftp@gmail.com>
2019-11-06 02:06:17 -0500

    spanScores only returning once.

--- extendables/spamScore.js
+++ extendables/spamScore.js
@@ -178,8 +178,8 @@ module.exports = class extends Extendable {
                     for (const key of Object.keys(body.attributeScores)) {
                         if (typeof body.attributeScores[ key ].spanScores !== 'undefined' && body.attributeScores[ key ].spanScores.length > 0) {
                             body.attributeScores[ key ].spanScores.map((spanScore) => {
-                                console.log(`spanScore ${key}: ${spanScore.score.value} / 0.95`)
-                                if (spanScore.score.value >= 0.95) {
+                                console.log(`spanScore ${key}: ${spanScore.score.value} / ${(1 - (this.cleanContent.length / 4000))}`)
+                                if (spanScore.score.value >= (1 - (this.cleanContent.length / 4000))) {
                                     switch (key) {
                                         case 'SEVERE_TOXICITY':
                                             score += 20;
