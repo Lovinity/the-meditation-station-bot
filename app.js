5595624f92b58e2bd3e43af73b09727c6088611e
Patrick Schmalstig <xanaftp@gmail.com>
2019-11-07 02:07:29 -0500

    startup changes

--- app.js
+++ app.js
@@ -1,159 +1,174 @@
-const { Client, PermissionLevels, Schema } = require('klasa');
-const MusicManager = require("./util/MusicManager");
-const config = require('./config.js');
-require('./util/extendTextChannel');
-require('./util/extendUser');
-require('./util/extendGuildMember');
-require('./util/extendGuild');
-
-// Guild schema
-Client.defaultGuildSchema
-    .add('botChannel', 'textchannel')
-    .add('botGamesChannel', 'textchannel')
-    .add('modLogChannel', 'textchannel')
-    .add('eventLogChannel', 'textchannel')
-    .add('flagLogChannel', 'textchannel')
-    .add('iceBreakerChannel', 'textchannel')
-    .add('announcementsChannel', 'textchannel')
-    .add('generalChannel', 'textchannel')
-    .add('starboardChannel', 'textchannel')
-    .add('noXPChannels', 'textchannel', { array: true })
-    .add('noSelfModRole', 'role')
-    .add('repEmoji', 'string')
-    .add('muteRole', 'role')
-    .add('modRole', 'role')
-    .add('raidRole', 'role')
-    .add('noRepRole', 'role')
-    .add('conflictResolutionMembers', 'integer', { default: 3, min: 1 })
-    .add('conflictResolutionTime', 'integer', { default: 15, min: 1 })
-    .add('verifiedRole', 'role')
-    .add('guildTasks', 'string', { configurable: false })
-    .add('perspectiveQueue', 'integer', { default: 0, min: 0, configurable: false })
-    .add('highestActivityScore', 'float', { configurable: false, default: 0, min: 0 })
-    .add('statsMessageChannel', 'string')
-    .add('statsMessage', 'string')
-    .add('reportMembers', 'integer', { default: 3, min: 1 })
-    .add('reportTime', 'integer', { default: 60, min: 1 })
-    .add('starboardRequired', 'integer', { default: 3, min: 1 })
-    .add('pendSuspensions', 'any', { array: true, configurable: false })
-    .add('pendBans', 'any', { array: true, configurable: false })
-    .add('pendIncidents', 'any', { array: true, configurable: false })
-    .add('incidentsCategory', 'categorychannel')
-    .add('staffCategory', 'categorychannel')
-    .add('antispamCooldown', 'integer', { default: 33, min: 0, max: 100 })
-    .add('antispamLessStrictRoles', 'role', { array: true })
-    .add('antispamLessStrictChannels', 'textchannel', { array: true })
-    .add('raidScore', 'integer', { default: 0, configurable: false })
-    .add('raidMitigation', 'integer', { default: 0, configurable: false })
-    .add('selfRoles', 'role', { array: true })
-    .add('levelRoles', folder => {
-        for (var i = 2; i <= 100; i++) {
-            folder.add(`level${i}`, 'role', { configurable: false });
+var exec = require('child_process').exec, child;
+child = exec('/usr/bin/java -jar ~/lovinity/Lavalink',
+    function (error, stdout, stderr) {
+        console.log('stdout: ' + stdout);
+        console.log('stderr: ' + stderr);
+        if (error !== null) {
+            console.log('exec error: ' + error);
+            process.exit(22);
         }
-    }, { configurable: false })
-    .add('music', folder => {
-        folder
-            .add('volume', 'integer', { min: 0, max: 100, default: 75 })
-    })
-    .add('yangStore', folder => {
-        folder
-            .add('generator', 'integer', { min: 0, default: 5 })
-            .add('nick', 'integer', { min: 0, default: 25 })
-            .add('profileTitle', 'integer', { min: 0, default: 50 })
-            .add('profileBackground', 'integer', { min: 0, default: 150 })
-            .add('profileColor', 'integer', { min: 0, default: 50 })
-            .add('remindme', 'integer', { min: 0, default: 25 })
-            .add('repMember', 'integer', { min: 0, default: 50 })
-            .add('8ball', 'integer', { min: 0, default: 5 })
-            .add('choice', 'integer', { min: 0, default: 5 })
-            .add('affirmations', 'integer', { min: 0, default: 25 })
-            .add('markov', 'integer', { min: 0, default: 10 })
-    })
-    .add('badges', 'any', { array: true, configurable: false });
+    });
 
-Client.use(require('klasa-member-gateway'));
+console.log(`Waiting 15 seconds for lavalink...`)
+setTimeout(() => {
+    const { Client, PermissionLevels, Schema } = require('klasa');
+    const MusicManager = require("./util/MusicManager");
+    const config = require('./config.js');
+    require('./util/extendTextChannel');
+    require('./util/extendUser');
+    require('./util/extendGuildMember');
+    require('./util/extendGuild');
 
-// Guild Member Schema
-Client.defaultMemberSchema
-    .add('xp', 'integer', { default: 0 })
-    .add('yang', 'integer', { default: 0 })
-    .add('badRep', 'integer', { default: 0 })
-    .add('goodRep', 'integer', { default: 0 })
-    .add('spamScore', 'integer', { default: 0 })
-    .add('activityScore', 'float', { min: 0, default: 0 })
-    .add('profile', folder => {
-        folder
-            .add('title', 'string')
-            .add('gender', 'string')
-            .add('pronouns', 'string')
-            .add('dob', 'string')
-            .add('location', 'string')
-            .add('factions', 'string')
-            .add('info', 'string')
-            .add('donations', 'float', { default: 0 })
-            .add('badges', 'url', { array: true })
-            .add('background', 'url')
-            .add('profileColor', folder2 => {
-                folder2
-                    .add('hue', 'float', { default: 0, min: 0, max: 0 })
-                    .add('saturation', 'float', { default: 0, min: 0, max: 100 })
-                    .add('lightness', 'float', { default: 100, min: 0, max: 1000 });
-            });
-    })
-    .add('canRep', 'boolean', { default: true })
-    .add('modLogs', 'any', { array: true })
-    .add('reports', 'string', { array: true })
-    .add('roles', 'role', { array: true });
+    // Guild schema
+    Client.defaultGuildSchema
+        .add('botChannel', 'textchannel')
+        .add('botGamesChannel', 'textchannel')
+        .add('modLogChannel', 'textchannel')
+        .add('eventLogChannel', 'textchannel')
+        .add('flagLogChannel', 'textchannel')
+        .add('iceBreakerChannel', 'textchannel')
+        .add('announcementsChannel', 'textchannel')
+        .add('generalChannel', 'textchannel')
+        .add('starboardChannel', 'textchannel')
+        .add('noXPChannels', 'textchannel', { array: true })
+        .add('noSelfModRole', 'role')
+        .add('repEmoji', 'string')
+        .add('muteRole', 'role')
+        .add('modRole', 'role')
+        .add('raidRole', 'role')
+        .add('noRepRole', 'role')
+        .add('conflictResolutionMembers', 'integer', { default: 3, min: 1 })
+        .add('conflictResolutionTime', 'integer', { default: 15, min: 1 })
+        .add('verifiedRole', 'role')
+        .add('guildTasks', 'string', { configurable: false })
+        .add('perspectiveQueue', 'integer', { default: 0, min: 0, configurable: false })
+        .add('highestActivityScore', 'float', { configurable: false, default: 0, min: 0 })
+        .add('statsMessageChannel', 'string')
+        .add('statsMessage', 'string')
+        .add('reportMembers', 'integer', { default: 3, min: 1 })
+        .add('reportTime', 'integer', { default: 60, min: 1 })
+        .add('starboardRequired', 'integer', { default: 3, min: 1 })
+        .add('pendSuspensions', 'any', { array: true, configurable: false })
+        .add('pendBans', 'any', { array: true, configurable: false })
+        .add('pendIncidents', 'any', { array: true, configurable: false })
+        .add('incidentsCategory', 'categorychannel')
+        .add('staffCategory', 'categorychannel')
+        .add('antispamCooldown', 'integer', { default: 33, min: 0, max: 100 })
+        .add('antispamLessStrictRoles', 'role', { array: true })
+        .add('antispamLessStrictChannels', 'textchannel', { array: true })
+        .add('raidScore', 'integer', { default: 0, configurable: false })
+        .add('raidMitigation', 'integer', { default: 0, configurable: false })
+        .add('selfRoles', 'role', { array: true })
+        .add('levelRoles', folder => {
+            for (var i = 2; i <= 100; i++) {
+                folder.add(`level${i}`, 'role', { configurable: false });
+            }
+        }, { configurable: false })
+        .add('music', folder => {
+            folder
+                .add('volume', 'integer', { min: 0, max: 100, default: 75 })
+        })
+        .add('yangStore', folder => {
+            folder
+                .add('generator', 'integer', { min: 0, default: 5 })
+                .add('nick', 'integer', { min: 0, default: 25 })
+                .add('profileTitle', 'integer', { min: 0, default: 50 })
+                .add('profileBackground', 'integer', { min: 0, default: 150 })
+                .add('profileColor', 'integer', { min: 0, default: 50 })
+                .add('remindme', 'integer', { min: 0, default: 25 })
+                .add('repMember', 'integer', { min: 0, default: 50 })
+                .add('8ball', 'integer', { min: 0, default: 5 })
+                .add('choice', 'integer', { min: 0, default: 5 })
+                .add('affirmations', 'integer', { min: 0, default: 25 })
+                .add('markov', 'integer', { min: 0, default: 10 })
+        })
+        .add('badges', 'any', { array: true, configurable: false });
 
-class lavaClient extends Client {
+    Client.use(require('klasa-member-gateway'));
 
-    constructor(...args) {
-        super(...args);
+    // Guild Member Schema
+    Client.defaultMemberSchema
+        .add('xp', 'integer', { default: 0 })
+        .add('yang', 'integer', { default: 0 })
+        .add('badRep', 'integer', { default: 0 })
+        .add('goodRep', 'integer', { default: 0 })
+        .add('spamScore', 'integer', { default: 0 })
+        .add('activityScore', 'float', { min: 0, default: 0 })
+        .add('profile', folder => {
+            folder
+                .add('title', 'string')
+                .add('gender', 'string')
+                .add('pronouns', 'string')
+                .add('dob', 'string')
+                .add('location', 'string')
+                .add('factions', 'string')
+                .add('info', 'string')
+                .add('donations', 'float', { default: 0 })
+                .add('badges', 'url', { array: true })
+                .add('background', 'url')
+                .add('profileColor', folder2 => {
+                    folder2
+                        .add('hue', 'float', { default: 0, min: 0, max: 0 })
+                        .add('saturation', 'float', { default: 0, min: 0, max: 100 })
+                        .add('lightness', 'float', { default: 100, min: 0, max: 1000 });
+                });
+        })
+        .add('canRep', 'boolean', { default: true })
+        .add('modLogs', 'any', { array: true })
+        .add('reports', 'string', { array: true })
+        .add('roles', 'role', { array: true });
 
-        this.lavalink = null;
-        this.music = new MusicManager();
+    class lavaClient extends Client {
+
+        constructor(...args) {
+            super(...args);
+
+            this.lavalink = null;
+            this.music = new MusicManager();
+        }
+
     }
 
-}
+    // Prepare Klasa
+    var client = new lavaClient({
+        clientOptions: {
+            fetchAllMembers: false
+        },
+        commandEditing: true,
+        typing: true,
+        ignoreBots: false,
+        slowmode: 5000,
+        slowmodeAggressive: true,
+        schedule: { interval: 5000 },
+        providers: {
+            default: 'mysql',
+            mysql: config.providers.mysql
+        },
+        // Add custom permissions
+        permissionLevels: new PermissionLevels()
+            // everyone can use these commands
+            .add(0, () => true)
+            // Members can view audit logs
+            .add(4, ({ guild, member }) => guild && member.permissions.has('VIEW_AUDIT_LOG'), { fetch: true })
+            // Members of guilds must have 'MANAGE_GUILD' permission
+            .add(6, ({ guild, member }) => guild && member.permissions.has('MANAGE_GUILD'), { fetch: true })
+            // The member using this command must be the guild owner
+            .add(7, ({ guild, member }) => guild && member === guild.owner, { fetch: true })
+            // Bot owner
+            .add(9, ({ author, client }) => author === client.owner, { break: true })
+            // Allows the bot owner to use Bot Owner only commands, which silently fail for other users.
+            .add(10, ({ author, client }) => author === client.owner),
+        readyMessage: (client) => `Ready to serve ${client.guilds.size} guilds and ${client.users.size} users`,
+    });
 
-// Prepare Klasa
-var client = new lavaClient({
-    clientOptions: {
-        fetchAllMembers: false
-    },
-    commandEditing: true,
-    typing: true,
-    ignoreBots: false,
-    slowmode: 5000,
-    slowmodeAggressive: true,
-    schedule: { interval: 5000 },
-    providers: {
-        default: 'mysql',
-        mysql: config.providers.mysql
-    },
-    // Add custom permissions
-    permissionLevels: new PermissionLevels()
-        // everyone can use these commands
-        .add(0, () => true)
-        // Members can view audit logs
-        .add(4, ({ guild, member }) => guild && member.permissions.has('VIEW_AUDIT_LOG'), { fetch: true })
-        // Members of guilds must have 'MANAGE_GUILD' permission
-        .add(6, ({ guild, member }) => guild && member.permissions.has('MANAGE_GUILD'), { fetch: true })
-        // The member using this command must be the guild owner
-        .add(7, ({ guild, member }) => guild && member === guild.owner, { fetch: true })
-        // Bot owner
-        .add(9, ({ author, client }) => author === client.owner, { break: true })
-        // Allows the bot owner to use Bot Owner only commands, which silently fail for other users.
-        .add(10, ({ author, client }) => author === client.owner),
-    readyMessage: (client) => `Ready to serve ${client.guilds.size} guilds and ${client.users.size} users`,
-});
+    // Add a channels gateway
+    client.gateways.register('channels', {
+        provider: 'mysql',
+        schema: new Schema()
+            .add('conflictResolution', 'string', { array: true })
+    });
 
-// Add a channels gateway
-client.gateways.register('channels', {
-    provider: 'mysql',
-    schema: new Schema()
-        .add('conflictResolution', 'string', { array: true })
-});
+    // login the client
+    client.login(config.botToken);
 
-// login the client
-client.login(config.botToken);
+}, 15000)
