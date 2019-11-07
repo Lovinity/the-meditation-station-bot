35e38fa1c48fab67a6050968441f7429f06b9eac
Patrick Schmalstig <xanaftp@gmail.com>
2019-11-07 04:59:15 -0500

    ...

---
+++ extendables/fetchURL.js
@@ -0,0 +1,19 @@
+const { Extendable, Piece } = require("klasa");
+const fetch = require("../util/fetch")
+
+module.exports = class extends Extendable {
+
+    constructor(...args) {
+        super(...args, { appliesTo: [ Piece ] });
+    }
+
+    fetchURL (url, options = {}) {
+        options.headers = options.headers ? { ...options.headers, "User-Agent": this.client.userAgent } : { "User-Agent": this.client.userAgent };
+        return fetch(url, options, options.type || "json").catch(error => {
+            Error.captureStackTrace(error);
+            this.client.emit("error", error);
+            throw error;
+        });
+    }
+
+};
