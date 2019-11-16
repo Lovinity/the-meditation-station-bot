b3f48e57bb889c8245b120bea39ea41114d71d54
Patrick Schmalstig <xanaftp@gmail.com>
2019-11-16 06:58:11 -0500

    ...

--- klasa-selfroles-gateway/src/lib/extensions/KlasaGuildMemberRoleStore.js
+++ klasa-selfroles-gateway/src/lib/extensions/KlasaGuildMemberRoleStore.js
@@ -6,18 +6,6 @@ const { GuildMemberRoleStore } = require('discord.js');
  */
 class KlasaGuildMemberRoleStore extends GuildMemberRoleStore {
 
-	async _fetchSingle(...args) {
-		const role = await super._fetchSingle(...args);
-		await role.settings.sync();
-		return role;
-	}
-
-	async _fetchMany(...args) {
-		const roles = await super._fetchMany(...args);
-		await Promise.all(roles.map(role => role.settings.sync()));
-		return roles;
-	}
-
 }
 
 module.exports = KlasaGuildMemberRoleStore;
