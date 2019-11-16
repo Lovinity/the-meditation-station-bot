443025ef71917ea6e7e0ba1259423ef900477495
Patrick Schmalstig <xanaftp@gmail.com>
2019-11-16 03:26:57 -0500

    Gotta debug my piss-poor code

--- commands/Moderation/selfroles.js
+++ commands/Moderation/selfroles.js
@@ -31,6 +31,7 @@ module.exports = class extends Command {
         if (!reaction) {
             return message.send(`:x: The selfroles command timed out, or no reaction was provided.`);
         }
+        console.dir(reaction.emoji)
         var settings = message.client.gateways.selfroles.get(`${role.id}`, true);
         if (!settings) {
             return message.send(`:x: There was an error getting settings for the provided role.`);
