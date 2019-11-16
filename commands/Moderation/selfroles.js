820dcfb863189468565b0250b5979293d81ec92b
Patrick Schmalstig <xanaftp@gmail.com>
2019-11-16 03:13:40 -0500

    ...

--- commands/Moderation/selfroles.js
+++ commands/Moderation/selfroles.js
@@ -9,7 +9,7 @@ module.exports = class extends Command {
             runIn: [ 'text' ],
             permLevel: 4,
             description: 'Manage reaction-based self-assignable roles',
-            usage: '<regenerate:default|add|remove> <role:rolename>',
+            usage: '<add|remove|regenerate:default> [role:rolename]',
             usageDelim: ' | ',
             cooldown: 15,
             requiredSettings: [ "selfRolesChannel" ],
@@ -17,6 +17,9 @@ module.exports = class extends Command {
     }
 
     async add (message, [ role ]) {
+        if (!role) {
+            return message.send(`:x: Role id/name/mention is required.`);
+        }
         var category = await message.awaitMessage(`:question: First, send a message containing the name of the category (case sentitive) you want this role to be added to. If you specify a category that does not exist, it will be created. Then, react to the message you just sent with the reaction you want users to use to assign that role to themselves.`, 60000);
         if (!category) {
             return message.send(`:x: The selfroles command timed out.`);
@@ -38,6 +41,9 @@ module.exports = class extends Command {
     }
 
     async remove (message, [ role ]) {
+        if (!role) {
+            return message.send(`:x: Role id/name/mention is required.`);
+        }
         var settings = message.client.gateways.selfroles.get(`${role.id}`);
         if (settings) { await settings.destroy() }
         return message.send(':white_check_mark: Self role removed! Once you have made all your changes, you must use the selfroles command with no parameters to re-generate the messages in the selfRolesChannel.')
