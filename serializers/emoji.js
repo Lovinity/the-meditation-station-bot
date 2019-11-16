ee012976da4cfd904e773dffbf6f4ce90c8b6b83
Patrick Schmalstig <xanaftp@gmail.com>
2019-11-16 04:36:00 -0500

    More debugging

--- serializers/emoji.js
+++ serializers/emoji.js
@@ -8,18 +8,25 @@ module.exports = class extends Serializer {
         if (data instanceof Emoji || data instanceof GuildEmoji || data instanceof ReactionEmoji) return data;
         if (typeof data !== 'string') throw this.constructor.error(language, piece.key);
 
+        console.log(`Data string`)
+
         var data = data.split(':');
 
         if (data.length > 1) {
+            console.log(`length > 1`)
             data = { name: data[ 0 ], id: data[ 1 ] }
+            console.dir(data)
         } else {
+            console.log(`length 1`)
             data = { name: String.fromCodePoint(parseInt(data[ 0 ])) }
+            console.dir(data)
         }
-
+        console.dir(new GuildEmoji(this.client, data, this.client.guilds.get(guild)));
         return new GuildEmoji(this.client, data, this.client.guilds.get(guild));
     }
 
     serialize (data) {
+        console.log(`Emoji serialise ${(data.id) ? `${data.name}:${data.id}` : data.name.codePointAt(0)}`)
         return (data.id) ? `${data.name}:${data.id}` : data.name.codePointAt(0);
     }
 
