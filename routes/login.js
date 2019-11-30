f9de84e21745592cffbf4af65d139d16fc7eb894
Patrick Schmalstig <xanaftp@gmail.com>
2019-11-30 18:54:22 -0500

    Try new authorization methods

---
+++ routes/login.js
@@ -0,0 +1,30 @@
+const { Route, util: { encrypt } } = require('klasa-dashboard-hooks');
+
+module.exports = class extends Route {
+
+    constructor(...args) {
+        super(...args, { route: 'login' });
+    }
+
+    async get (request, response) {
+
+        if (!request.query.state) return response.end(JSON.stringify({ error: "state was not specified" }));
+        if (!request.query.redirect_uri) return response.end(JSON.stringify({ error: "redirect_uri was not specified" }));
+
+        if (!request.query.redirect_uri.startsWith(this.client.options.dashboardHooks.origin)) return response.end(JSON.stringify({ error: "Disallowed redirect_uri" }));
+
+        var state = encrypt({
+            ID: request.query.state,
+            redirect: request.query.redirect_uri
+        }, this.client.options.clientSecret);
+
+        var discordURL = `https://discordapp.com/api/oauth2/authorize?client_id=${this.client.options.clientID}&redirect_uri=${encodeURI(`${this.client.options.dashboardHooks.origin}/oauth/callback`)}&response_type=code&scope=identify&state=${state}`
+
+        response.writeHead(302,
+            { Location: discordURL }
+        );
+        
+        return response.end();
+    }
+
+};
--- routes/oauthCallback.js
+++ routes/oauthCallback.js
@@ -1,4 +1,4 @@
-const { Route, util: { encrypt }, constants: { RESPONSES } } = require('klasa-dashboard-hooks');
+const { Route, util: { encrypt, decrypt }, constants: { RESPONSES } } = require('klasa-dashboard-hooks');
 const fetch = require('node-fetch');
 
 module.exports = class extends Route {
@@ -7,55 +7,31 @@ module.exports = class extends Route {
 		super(...args, { route: 'oauth/callback' });
 	}
 
-	get oauthUser() {
+	get oauthUser () {
 		return this.store.get('oauthUser');
 	}
 
-	async post(request, response) {
-		/* eslint-disable camelcase */
-		if (!request.body.code) return this.noCode(response);
-		const url = new URL('https://discordapp.com/api/oauth2/token');
-		url.search = new URLSearchParams([
-			['grant_type', 'authorization_code'],
-			['redirect_uri', request.body.redirectUri || this.client.options.dashboardHooks.redirectUri],
-			['code', request.body.code]
-		]);
-		const res = await fetch(url, {
-			headers: { Authorization: `Basic ${Buffer.from(`${this.client.options.clientID}:${this.client.options.clientSecret}`).toString('base64')}` },
-			method: 'POST'
-		});
-		if (!res.ok) return response.end(RESPONSES.FETCHING_TOKEN);
-
-		const { oauthUser } = this;
-
-		if (!oauthUser) return this.notReady(response);
-
-		const body = await res.json();
-		const user = await oauthUser.api(body.access_token);
-
-		return response.end(JSON.stringify({
-			access_token: encrypt({
-				token: body.access_token,
-				scope: [user.id, ...user.guilds.filter(guild => guild.userCanManage).map(guild => guild.id)]
-			}, this.client.options.clientSecret),
-			user
-		}));
-		/* eslint-enable camelcase */
-    }
-    
-    async get(request, response) {
+	async get (request, response) {
 		/* eslint-disable camelcase */
 		if (!request.query.code) return this.noCode(response);
+		if (!request.query.state) return response.send(JSON.stringify({ error: "State was not returned." }));
+
+		try {
+			var state = decrypt(request.query.state, this.client.options.clientSecret);
+		} catch (e) {
+			return response.send(JSON.stringify({ error: "Unable to decrypt returned state. Clickjacking might have occurred!" }))
+		}
+
 		const url = new URL('https://discordapp.com/api/oauth2/token');
 		url.search = new URLSearchParams([
-			['grant_type', 'authorization_code'],
-			['redirect_uri', request.query.redirectUri || this.client.options.dashboardHooks.redirectUri],
-			['code', request.query.code]
+			[ 'grant_type', 'authorization_code' ],
+			[ 'redirect_uri', request.query.redirectUri || this.client.options.dashboardHooks.redirectUri ],
+			[ 'code', request.query.code ]
 		]);
 		var res = await fetch(url, {
 			headers: { Authorization: `Basic ${Buffer.from(`${this.client.options.clientID}:${this.client.options.clientSecret}`).toString('base64')}` },
 			method: 'POST'
-        });
+		});
 
 		if (!res.ok) return response.end(RESPONSES.FETCHING_TOKEN);
 
@@ -66,22 +42,25 @@ module.exports = class extends Route {
 		const body = await res.json();
 		const user = await oauthUser.api(body.access_token);
 
-		return response.end(JSON.stringify({
-			access_token: encrypt({
-				token: body.access_token,
-				scope: [user.id, ...user.guilds.filter(guild => guild.userCanManage).map(guild => guild.id)]
-			}, this.client.options.clientSecret),
-			user
-		}));
-        /* eslint-enable camelcase */
+		const access_token = encrypt({
+			token: body.access_token,
+			scope: [ user.id, ...user.guilds.filter(guild => guild.userCanManage).map(guild => guild.id) ]
+		}, this.client.options.clientSecret);
+
+		const redirectURL = `${state.redirect}#access_token=${access_token}&state=${state.ID}&tag=${encodeURI(user.tag)}&avatar=${encodeURI(user.displayAvatarURL({ format: 'png' }))}`;
+
+		response.writeHead(302,
+			{ Location: redirectURL }
+		);
+		return response.end();
 	}
 
-	notReady(response) {
+	notReady (response) {
 		response.writeHead(500);
 		return response.end(RESPONSES.NOT_READY);
 	}
 
-	noCode(response) {
+	noCode (response) {
 		response.writeHead(400);
 		return response.end(RESPONSES.NO_CODE);
 	}
