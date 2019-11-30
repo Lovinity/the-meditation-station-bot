0cae1127d5d7090e9f27f32e3013efc1bcf3c95f
Patrick Schmalstig <xanaftp@gmail.com>
2019-11-30 11:02:21 -0500

    Lose canvas profiles; generate profile URL instead

--- app.js
+++ app.js
@@ -91,7 +91,7 @@ Client.defaultMemberSchema
             .add('location', 'string')
             .add('info', 'string')
             .add('donations', 'float', { default: 0 })
-            .add('badges', 'url', { array: true })
+            .add('badges', 'integer', { array: true })
             .add('background', 'url')
             .add('profileColor', folder2 => {
                 folder2
