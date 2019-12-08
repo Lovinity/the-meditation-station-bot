672a97fba1b8b7cc3225eef32a8cae653967fdb0
Patrick Schmalstig <xanaftp@gmail.com>
2019-12-07 22:35:14 -0500

    Archive command

---
+++ commands/Moderation/archive.js
@@ -0,0 +1,37 @@
+const { Command } = require('klasa');
+
+module.exports = class extends Command {
+
+    constructor(...args) {
+        super(...args, {
+            permissionLevel: 5,
+            botPerms: [ 'MANAGE_CHANNELS' ],
+            runIn: [ 'text' ],
+            description: 'Clone the text channel and then set the original channel to read-only by administrators.',
+            usage: '',
+            usageDelim: ''
+        });
+    }
+
+    async run (message, []) {
+
+        var newChannel = await message.channel.clone({ options: { reason: "Channel archive" } });
+
+        await message.channel.overwritePermissions({
+            permissionOverwrites: [
+                {
+                    id: message.channel.guild.roles.everyone,
+                    deny: [ 'VIEW_CHANNEL' ],
+                },
+            ],
+            reason: 'Channel archive'
+        });
+
+        await message.channel.edit({name: `${message.channel.name}-archived`});
+
+        await newChannel.send(`:exclamation: This is a cloned channel; the original channel by the same name has been archived by staff and can no longer be accessed by regular members.`);
+
+        return message.send(':white_check_mark: This channel has been archived.')
+    }
+
+};
