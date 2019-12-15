2c2bcc209c34a44d7eaf6f15c86f49fa04556a04
Patrick Schmalstig <xanaftp@gmail.com>
2019-12-15 11:58:27 -0500

    package updates

--- events/guildMemberSpeaking.js
+++ events/guildMemberSpeaking.js
@@ -2,13 +2,7 @@ const { Event } = require('klasa');
 
 module.exports = class extends Event {
 
-    constructor(...args) {
-        super(...args, {
-            enabled: true,
-        });
-    }
-
-    async run (guildMember, speaking) {
+    run (guildMember, speaking) {
         console.log(`Speaking ${guildMember.id}? ${speaking}`)
     }
 
