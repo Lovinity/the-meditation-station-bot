ec4c825510cd4ab02b4c01f9f8deb7455d353d8f
Patrick Schmalstig <xanaftp@gmail.com>
2019-12-17 03:31:40 -0500

    muted role should not get saved in the list of roles

--- events/guildMemberUpdate.js
+++ events/guildMemberUpdate.js
@@ -2,31 +2,34 @@ const { Event } = require('klasa');
 
 module.exports = class extends Event {
 
-    run (oldMember, newMember) {
-        var isMuted = (newMember.roles.get(newMember.guild.settings.muteRole));
-        var wasMuted = (oldMember.roles.get(oldMember.guild.settings.muteRole));
+    async run (oldMember, newMember) {
+        const mutedRole = newMember.guild.roles.resolve(newMember.guild.settings.muteRole);
+        if (mutedRole) {
+            var isMuted = (newMember.roles.get(newMember.guild.settings.muteRole));
+            var wasMuted = (oldMember.roles.get(oldMember.guild.settings.muteRole));
 
-        // Kick the user out of voice channels if they are muted
-        if (isMuted && newMember.voice.channelID) {
-            newMember.voice.kick(`User is muted`)
-        }
+            // Kick the user out of voice channels if they are muted
+            if (isMuted && newMember.voice.channelID) {
+                newMember.voice.kick(`User is muted`)
+            }
 
-        // If newly muted, or muted with more than 1 role, or not muted when they should be muted, remove all roles except muted.
-        if ((!wasMuted && isMuted) || (isMuted && newMember.roles.size > 1) || (!isMuted && !wasMuted && newMember.settings.muted)) {
-            newMember.settings.update(`muted`, true, newMember.guild);
-            // Remove all roles except the muted role
-            newMember.roles.set([ newMember.guild.settings.muteRole ], `User muted; remove all other roles`);
+            // If newly muted, or muted with more than 1 role, or not muted when they should be muted, remove all roles except muted.
+            if ((!wasMuted && isMuted) || (isMuted && newMember.roles.size > 1) || (!isMuted && !wasMuted && newMember.settings.muted)) {
+                await newMember.settings.update(`muted`, true, newMember.guild);
+                // Remove all roles except the muted role
+                newMember.roles.set([ newMember.guild.settings.muteRole ], `User muted; remove all other roles`);
 
-        } else if (wasMuted && !isMuted) { // User was muted and is no longer muted; re-assign roles.
-            newMember.settings.update(`muted`, false, newMember.guild);
-            newMember.roles.set(newMember.settings.roles, `User no longer muted; apply previous roles`);
+            } else if (wasMuted && !isMuted) { // User was muted and is no longer muted; re-assign roles.
+                await newMember.settings.update(`muted`, false, newMember.guild);
+                newMember.roles.set(newMember.settings.roles, `User no longer muted; apply previous roles`);
 
-        } else if (!isMuted && !wasMuted) { // User not, nor was, muted; update role database
-            newMember.settings.reset(`roles`);
-            newMember.roles.each((role) => {
-                if (role.id !== newMember.guild.roles.everyone.id)
-                    newMember.settings.update(`roles`, role, newMember.guild, { action: 'add' });
-            });
+            } else if (!isMuted && !wasMuted) { // User not, nor was, muted; update role database
+                newMember.settings.reset(`roles`);
+                newMember.roles.each((role) => {
+                    if (role.id !== newMember.guild.roles.everyone.id && role.id !== newMember.guild.settings.muteRole)
+                        newMember.settings.update(`roles`, role, newMember.guild, { action: 'add' });
+                });
+            }
         }
     }
 
