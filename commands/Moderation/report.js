3ef4091944ffa699363861c4077b649ff0cd61e3
Patrick Schmalstig <xanaftp@gmail.com>
2019-12-18 14:08:32 -0500

    Unforeseen bug

--- commands/Moderation/report.js
+++ commands/Moderation/report.js
@@ -39,15 +39,15 @@ module.exports = class extends Command {
         const guildMember = message.guild.members.resolve(user.id);
         const incidents = message.guild.settings.incidentsCategory;
 
+        // Reporter is not allowed to report.
+        if (message.member.roles.get(noSelfModRole.id))
+            return message.send(`:x: Sorry, but you are not allowed to use the report command due to past abuse. You can still post evidence of the member violating the rules in this channel for staff to investigate.`);
+
         if (!guildMember)
-        return message.send(`:x: The member you were trying to report is no longer in the guild, so I could not acknowledge your report. However, you can still provide evidence here for staff to investigate; staff can still take action despite the member having left the guild.`);
+            return message.send(`:x: The member you were trying to report is no longer in the guild, so I could not acknowledge your report. However, you can still provide evidence here for staff to investigate; staff can still take action despite the member having left the guild.`);
 
         // Check if this specific member used the conflict command on the user recently. If not, add an entry.
         if (reports.indexOf(`${message.author.id}`) === -1) {
-
-            // Reporter is not allowed to report.
-            if (guildMember && guildMember.roles.get(noSelfModRole.id))
-            return message.send(`:x: Sorry, but you are not allowed to use the report command due to past abuse. You can still post evidence of the member violating the rules in this channel for staff to investigate.`);
 
             // Reported member is already muted.
             if (guildMember && guildMember.roles.get(mutedRole.id))
