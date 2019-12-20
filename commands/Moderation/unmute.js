b1994549e6376854446e78c2a278b619d1bdc53b
Patrick Schmalstig <xanaftp@gmail.com>
2019-12-20 00:28:50 -0500

    unmute command

---
+++ commands/Moderation/unmute.js
@@ -0,0 +1,49 @@
+const { Command } = require('klasa');
+
+module.exports = class extends Command {
+
+    constructor(...args) {
+        super(...args, {
+            permissionLevel: 5,
+            botPerms: [ 'MANAGE_ROLES' ],
+            runIn: [ 'text' ],
+            description: 'Unmute a member.',
+            usage: '<user:username>',
+            usageDelim: ''
+        });
+    }
+
+    async run (message, [ user ]) {
+
+        const muted = message.guild.settings.muteRole;
+        const mutedRole = message.guild.roles.resolve(muted);
+        var guildMember = message.guild.members.resolve(user.id);
+
+        if (!mutedRole)
+            return message.send(`:x: This guild does not have a muteRole. Please create a muteRole to use the unmute command.`);
+
+        if (guildMember) {
+            if (!guildMember.roles.get(muted))
+                return message.send(`:x: That user is not currently muted.`);
+
+            guildMember.roles.remove(mutedRole, `Mute removed with !unmute by ${message.author.tag}`);
+            const channel = guild.channels.resolve(guild.settings.generalChannel);
+            if (channel) {
+                channel.send(`:loud_sound: <@${user.id}> , your mute has been removed by staff. You can now participate in the guild again.`);
+            }
+        } else {
+            if (!user.guildSettings(message.guild.id).muted)
+                return message.send(`:x: That user is not currently muted.`);
+        }
+
+        user.guildSettings(message.guild.id).update(`muted`, false, message.guild);
+
+        const channel2 = guild.channels.resolve(guild.settings.modLogChannel);
+        if (channel2) {
+            channel2.send(`:loud_sound: Member ${user.tag} (${user.id}) was unmuted by ${message.author.tag}.`);
+        }
+
+        return message.send(`:white_check_mark: User was unmuted!`);
+    }
+
+};
