a5f991fab23884386f1656e2183538faebe7e7aa
Patrick Schmalstig <xanaftp@gmail.com>
2019-12-27 15:49:22 -0500

    Try non-bulk pruning

--- commands/Moderation/prune.js
+++ commands/Moderation/prune.js
@@ -10,13 +10,13 @@ module.exports = class extends Command {
             botPerms: ['MANAGE_MESSAGES'],
             runIn: ['text'],
             description: 'Prune messages',
-            usage: '[limit:integer{1,1000}] [link|invite|bots|you|me|upload|noupload|user:username]',
+            usage: '[limit:integer{1,300}] [link|invite|bots|you|me|upload|noupload|user:username]',
             usageDelim: ' | '
         });
     }
 
     async run(message, [limit = 50, filter = null]) {
-        var message = await message.send(`:hourglass_flowing_sand: Pruning messages (This might take a while)...`);
+        var message = await message.send(`:hourglass_flowing_sand: Pruning messages (This might take several minutes)...`);
         await this.process(message, limit, filter);
         return message.send(`:white_check_mark: I'm done pruning! All clean!`);
     }
@@ -48,30 +48,36 @@ module.exports = class extends Command {
     async process(message, limit, filter) {
         wait.for.time(3);
         var iteration = 0;
+        var before = message.id;
         while (limit > 0 && iteration < 10)
         {
-            var filtered = await this._process(message, limit, filter);
-            if (filtered <= 0)
+            var filtered = await this._process(message, limit, filter, before);
+            if (filtered[0] <= 0)
                 limit = -1;
+            before = filtered[1];
             limit -= filtered;
-            wait.for.time(10);
+            wait.for.time(5);
             iteration++;
         }
         return true;
     }
 
-    async _process(message, amount, filter) {
-        let messages = await message.channel.messages.fetch({limit: 100});
+    async _process(message, amount, filter, before) {
+        let messages = await message.channel.messages.fetch({limit: 100, before: before});
         if (messages.array().length <= 0)
-            return -1;
+            return [-1];
+        before = messages.firstKey();
         if (filter) {
             const user = typeof filter !== 'string' ? filter : null;
             const type = typeof filter === 'string' ? filter : 'user';
             messages = messages.filter(this.getFilter(message, type, user));
         }
         messages = messages.array().slice(0, amount);
-        await message.channel.bulkDelete(messages);
-        return messages.length;
+        messages.map((msg) => {
+            msg.delete();
+            wait.for.time(1);
+        });
+        return [messages.length, before];
     }
 
 };
