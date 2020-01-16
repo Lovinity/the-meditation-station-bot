9f8924c8865f3ff5e651173b4ac55f0e6e0111c6
Patrick Schmalstig <xanaftp@gmail.com>
2020-01-15 21:30:11 -0500

    try making a GuildMember

--- util/extendUser.js
+++ util/extendUser.js
@@ -1,4 +1,4 @@
-const { Structures } = require('discord.js');
+const { Structures, GuildMember } = require('discord.js');
 
 Structures.extend('User', User => class MyUser extends User {
 
@@ -7,15 +7,27 @@ Structures.extend('User', User => class MyUser extends User {
 
     // Guild based member settings
     this.guildSettings = (guildID) => {
-      return this.client.gateways.members.get(`${guildID}.${this.id}`, true);
+      var guild = this.client.guilds.resolve(guildID);
+      if (!guild) return null;
+
+      var guildmember = new GuildMember(this.client, {
+        user: this
+      }, guild);
+
+      return guildmember.settings;
     };
 
     this.HP = (guildID) => {
-      var settings = this.client.gateways.members.get(`${guildID}.${this.id}`, true);
       var guild = this.client.guilds.resolve(guildID);
       if (!guild) return 100;
 
-      var damage =settings.HPDamage;
+      var guildmember = new GuildMember(this.client, {
+        user: this
+      }, guild);
+
+      var settings = guildmember.settings;
+
+      var damage = settings.HPDamage;
       var decay = guild.settings.oneHPPerXP;
       var HP = (100 + Math.floor(decay > 0 ? settings.xp / decay : 0)) - damage;
       if (HP < 0) HP = 0;
