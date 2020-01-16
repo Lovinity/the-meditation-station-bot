5e5a01276a3431f4ac1e88cde76a46b0a778e7d7
Patrick Schmalstig <xanaftp@gmail.com>
2020-01-16 00:02:21 -0500

    dammit

--- member-gateway/src/lib/extensions/KlasaGuildMemberStore.js
+++ member-gateway/src/lib/extensions/KlasaGuildMemberStore.js
@@ -8,16 +8,13 @@ class KlasaGuildMemberStore extends GuildMemberStore {
 
 	async _fetchSingle(...args) {
 		const member = await super._fetchSingle(...args);
-		member.settings = await member.settings.sync();
+		await member.settings.sync();
 		return member;
 	}
 
 	async _fetchMany(...args) {
 		const members = await super._fetchMany(...args);
-		await Promise.all(members.map(async member => {
-			member.settings = await member.settings.sync();
-			return member;
-		}));
+		await Promise.all(members.map(member => member.settings.sync()));
 		return members;
 	}
 
