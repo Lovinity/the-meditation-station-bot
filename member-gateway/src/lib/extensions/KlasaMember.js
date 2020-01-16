98bb488c38a0545edf162d1c646373058d01cc9d
Patrick Schmalstig <xanaftp@gmail.com>
2020-01-16 00:04:09 -0500

    well that didn't work either...

--- member-gateway/src/lib/extensions/KlasaMember.js
+++ member-gateway/src/lib/extensions/KlasaMember.js
@@ -5,23 +5,19 @@ module.exports = Structures.extend('GuildMember', GuildMember => {
 	 * Klasa's Extended GuildMember
 	 * @extends external:GuildMember
 	 */
-    class KlasaMember extends GuildMember {
+	class KlasaMember extends GuildMember {
 
-        constructor(...args) {
-            super(...args);
-        }
+		constructor(...args) {
+			super(...args);
 
-        toJSON () {
-            return { ...super.toJSON(), settings: this.settings };
-        }
+            this.settings = this.client.gateways.members.get(`${this.guild.id}.${this.id}`, true);
+		}
 
-        get settings () {
-            var settings = this.client.gateways.members.get(`${this.guild.id}.${this.id}`, true);
-            settings.sync();
-            return settings;
-        }
+		toJSON() {
+			return { ...super.toJSON(), settings: this.settings };
+		}
 
-    }
+	}
 
-    return KlasaMember;
+	return KlasaMember;
 });
