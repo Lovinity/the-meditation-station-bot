89f0a78d2c3cb1ff445ade9db6537ce7fbc51e28
Patrick Schmalstig <xanaftp@gmail.com>
2020-02-01 02:45:30 -0500

    Test audit log fetching

--- events/channelDelete.js
+++ events/channelDelete.js
@@ -23,6 +23,17 @@ module.exports = class extends Event {
 
         const _channel = this.client.channels.resolve(modLog);
 
+        // Find out who deleted the channel
+        const fetchedLogs = await channel.guild.fetchAuditLogs({
+            limit: 1,
+            type: 'CHANNEL_DELETE',
+        });
+        const auditLog = fetchedLogs.entries.first();
+        if (auditLog)
+            const { auditExecutor, auditTarget } = auditLog;
+        if (auditTarget.id !== channel.id)
+            auditLog = undefined;
+
         // Initiate data variable
         var data = `ARCHIVE (max 1,000 messages / 10 days old) of deleted text channel ${channel.name}, ID ${channel.id}\nCreated on ${moment(channel.createdAt).format()}\nDeleted on ${moment().format()}\n\n`;
 
@@ -50,7 +61,7 @@ module.exports = class extends Event {
         var buffer = new Buffer(data, "utf-8");
 
         // Send the buffer to the staff channel as a txt file
-        _channel.send(`:speech_left: :wastebasket: The channel ${channel.name} (${channel.id}) was deleted.`, { files: [ { attachment: buffer, name: `${channel.name}.txt` } ] });
+        _channel.send(`:speech_left: :wastebasket: The channel ${channel.name} (${channel.id}) was deleted${auditLog ? ` by ${auditExecutor.tag}` : ``}.`, { files: [ { attachment: buffer, name: `${channel.name}.txt` } ] });
 
 
     }
